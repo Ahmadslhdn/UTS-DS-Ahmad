6ae5661b0b95e8b55b2c41f3c407ff5fc79442f1
Ahmadslhdn <116964488+Ahmadslhdn@users.noreply.github.com>
2022-11-20 17:44:20 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("No 1");
+        int arka[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+        System.out.println("bilangan genap");
+        for (int i = 0; i< arka.length; i++){
+            if (arka[i]%2 == 0){
+                System.out.println(arka[i]);
+            }
+        }
+        System.out.println("bilangan ganjil");
+        for (int i =0; i<arka.length; i++){
+            if (arka[i]%2!= 0){
+                System.out.println(arka[i]);
+            }
+        }
+    }
+}
